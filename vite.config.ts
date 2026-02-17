09e306fea69ff3b50da4b024f774b6993712b50c
oh in seong <58154638+ois0886@users.noreply.github.com>
2026-02-17 17:41:59 +0900

    feat: 코드 하이라이트 추가

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,13 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/Oh-In-Seong-Portfolio/',
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'syntax-highlighter': ['react-syntax-highlighter'],
+        },
+      },
+    },
+  },
 })
